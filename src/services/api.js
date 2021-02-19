fdc74c2e86b8897e2f56cfec0d3e7d057f2cb8b4
Roie Natan <roie.natan@gmail.com>
2021-02-19 19:16:08 +0200

    use relative APIs

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,19 @@
 export const API = {
-    LOGIN: 'http://localhost:3001/authenticate/login',
-    GET_COUNTRIES: 'http://localhost:3001/countries/getCountries',
-    ADD_COUNTRY: 'http://localhost:3001/countries/addCountry',
-    GET_IMAGE: 'http://localhost:3001/images/getImage/',
-    GET_COUNTRY_IMAGES: 'http://localhost:3001/images/getCountryImages/',
-    ADD_IMAGE: 'http://localhost:3001/images/addImage',
+    LOGIN: '/authenticate/login',
+    GET_COUNTRIES: '/countries/getCountries',
+    ADD_COUNTRY: '/countries/addCountry',
+    GET_IMAGE: '/images/getImage/',
+    GET_COUNTRY_IMAGES: '/images/getCountryImages/',
+    ADD_IMAGE: '/images/addImage',
 };
+
+/* LOCAL DEV */
+
+// export const API = {
+//   LOGIN: 'http://localhost:3001/authenticate/login',
+//   GET_COUNTRIES: 'http://localhost:3001/countries/getCountries',
+//   ADD_COUNTRY: 'http://localhost:3001/countries/addCountry',
+//   GET_IMAGE: 'http://localhost:3001/images/getImage/',
+//   GET_COUNTRY_IMAGES: 'http://localhost:3001/images/getCountryImages/',
+//   ADD_IMAGE: 'http://localhost:3001/images/addImage',
+// };
